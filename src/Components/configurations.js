34e7b8b2041a0e1fbddbe4de4bb06d4674e10589
sudipjha5@gmail.com <sudipjha5@gmail.com>
2024-03-22 13:05:23 +0530

    Changed the backend url in conf

--- src/Components/configurations.js
+++ src/Components/configurations.js
@@ -1,4 +1,4 @@
-//const backend_url = "https://docstorage-server-e48dce0ce08a.herokuapp.com/"
-const backend_url = "http://127.0.0.1:8000/"
+const backend_url = "https://docstorage-server-e48dce0ce08a.herokuapp.com/"
+//const backend_url = "http://127.0.0.1:8000/"
 
 export { backend_url}
